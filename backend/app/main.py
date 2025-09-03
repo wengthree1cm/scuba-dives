1a703f96effaebfcac1f595eb1e2daa16ddd8397
wengthree1cm <chl135@ucsd.edu>
2025-09-02 23:09:42 -0400

    sd

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://scuba-dives-page.onrender.com",
+        "https://scuba-dives.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
